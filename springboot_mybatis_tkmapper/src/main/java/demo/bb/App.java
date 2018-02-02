d41b02a3c683b19c242820c6c0838ac4bbcbe2e2
015831 <434534658@qq.com>
2018-02-02 13:26:19 +0800

    123

--- springboot_mybatis_tkmapper/src/main/java/demo/bb/App.java
+++ springboot_mybatis_tkmapper/src/main/java/demo/bb/App.java
@@ -1,6 +1,5 @@
 package demo.bb;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
